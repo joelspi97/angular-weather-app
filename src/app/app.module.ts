d1f2f83d5ef028bd1d4faa9b11b206c7e7a17cdb
Joel Spinelli <joelspi97@gmail.com>
2022-02-22 23:18:50 -0300

    adding initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { WeatherComponent } from './pages/weather/weather.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SearchComponent,
+    WeatherComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
