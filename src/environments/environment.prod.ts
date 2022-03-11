62e3d6b9f8081171cf369ba8a47ba872cb2b60da
Joel Spinelli <joelspi97@gmail.com>
2022-03-11 12:17:36 -0300

    fixed secret leak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   openWeather: {
-    key: 'bfe1cc125b85e629fd8376d5d7e8e82c',
+    key: 'insert API key here',
     url: 'https://api.openweathermap.org/data/2.5/'
   }
 };
